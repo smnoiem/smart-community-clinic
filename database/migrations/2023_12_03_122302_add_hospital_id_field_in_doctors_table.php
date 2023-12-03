2f933e43ba95abb88434b1a210d4613045dd54de
S M Noiem Uddin <smnoiem@gmail.com>
2023-12-03 18:27:34 +0600

    added foreign keys in practitioners, doctors, and clinics migrations

---
+++ database/migrations/2023_12_03_122302_add_hospital_id_field_in_doctors_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('doctors', function (Blueprint $table) {
+            $table->foreignId("hospital_id")->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('doctors', function (Blueprint $table) {
+            $table->dropForeign(["hospital_id"]);
+            $table->dropColumn("hospital_id");
+        });
+    }
+};
