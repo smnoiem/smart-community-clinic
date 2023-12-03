db7824de51f9dd28b828ebe3b31a3c4de7522122
S M Noiem Uddin <smnoiem@gmail.com>
2023-12-03 18:38:50 +0600

    added migration for visits table

---
+++ database/migrations/2023_12_03_122807_create_visits_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visits', function (Blueprint $table) {
+
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+            $table->id();
+            $table->string('patient_name', 100);
+            $table->integer('age');
+            $table->string('address', 200);
+            $table->string('nid', 200);
+            $table->string('status', 50);
+            $table->boolean('need_follow_up');
+            $table->boolean('need_ambulance');
+            $table->boolean('need_transfer');
+
+            $table->foreignId("clinic_id")->constrained();
+            $table->foreignId("practitioner_id")->constrained();
+
+            $table->unsignedBigInteger("parent_visit_id");
+            $table->foreign("parent_visit_id")->references("id")->on("visits");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visits');
+    }
+};
