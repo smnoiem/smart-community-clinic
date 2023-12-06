345df451e62cbda0bc0a554a188b7adc58ec8c43
S M Noiem Uddin <smnoiem@gmail.com>
2023-12-07 02:09:54 +0600

    displayed doctors, clinic, practitioner, and hospitals dynamically

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Clinic;
+use App\Models\Doctor;
+use App\Models\Hospital;
+use App\Models\Practitioner;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index() {
-        return view('admin.dashboard.index');
+
+        $topHospitals = Hospital::latest()->take(5)->get();
+        $topDoctors = Doctor::latest()->take(5)->get();
+        $topClinics = Clinic::latest()->take(5)->get();
+        $topPractitioners = Practitioner::latest()->take(5)->get();
+
+        return view('admin.dashboard.index', compact('topHospitals', 'topDoctors', 'topClinics', 'topPractitioners'));
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -15,6 +15,15 @@
                     <div class="card-body">
                         <h4 class="card-title">Hospital</h4>
                         <p class="card-text">Top hospitals</p>
+
+                        <ul class="list-group list-group-flush">
+                            @foreach ($topHospitals as $hospital)
+                                <li class="list-group-item"><a href="{{ route('hospitals.show', $hospital->id) }}">{{ $hospital->name }}</a></li>
+                            @endforeach
+                        </ul>
+
+                        <br>
+
                         <a href="{{ route('hospitals.create') }}" class="card-link">Create Hospital</a>
                         <a href="{{ route('hospitals.index') }}" class="card-link">See all</a>
                     </div>
@@ -27,6 +36,15 @@
                     <div class="card-body">
                         <h4 class="card-title">Doctors</h4>
                         <p class="card-text">Doctor list</p>
+
+                        <ul class="list-group list-group-flush">
+                            @foreach ($topDoctors as $doctor)
+                                <li class="list-group-item"><a href="{{ route('doctors.show', $doctor->id) }}">{{ $doctor->name }}</a></li>
+                            @endforeach
+                        </ul>
+
+                        <br>
+
                         <a href="{{ route('doctors.create') }}" class="card-link">Add doctor</a>
                         <a href="{{ route('doctors.index') }}" class="card-link">See all</a>
                     </div>
@@ -38,6 +56,15 @@
                     <div class="card-body">
                         <h4 class="card-title">Clinic</h4>
                         <p class="card-text">Top Clinics</p>
+
+                        <ul class="list-group list-group-flush">
+                            @foreach ($topClinics as $clinic)
+                                <li class="list-group-item"><a href="{{ route('clinics.show', $clinic->id) }}">{{ $clinic->name }}</a></li>
+                            @endforeach
+                        </ul>
+
+                        <br>
+
                         <a href="{{ route('clinics.create') }}" class="card-link">Create Clinic</a>
                         <a href="{{ route('clinics.index') }}" class="card-link">See all</a>
                     </div>
@@ -49,6 +76,15 @@
                     <div class="card-body">
                         <h4 class="card-title">Practitioner</h4>
                         <p class="card-text">Top practitioners</p>
+
+                        <ul class="list-group list-group-flush">
+                            @foreach ($topPractitioners as $practitioner)
+                                <li class="list-group-item"><a href="{{ route('practitioners.show', $practitioner->id) }}">{{ $practitioner->name }}</a></li>
+                            @endforeach
+                        </ul>
+
+                        <br>
+
                         <a href="{{ route('practitioners.create') }}" class="card-link">Create practitioner</a>
                         <a href="{{ route('practitioners.index') }}" class="card-link">See all</a>
                     </div>
